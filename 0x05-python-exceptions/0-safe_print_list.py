4b865ab6e9993aead433279b328f0513f3ad8430
EricoMartin <ericomartin.0785@gmail.com>
2023-09-25 12:12:47 -0700

    0x05-python-exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,6 +8,7 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[i], end="")
         except ValueError as exc:
             print(type(exc))
+            break
         finally:
             print()
-            return num
+    return num
