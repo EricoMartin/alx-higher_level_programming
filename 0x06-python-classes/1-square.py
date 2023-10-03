0923bbd885dcb951ee75185f5c3cc92ecb6e300a
EricoMartin <ericomartin.0785@gmail.com>
2023-10-03 08:44:15 -0700

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,7 +5,7 @@
 class Square:
     """This class defines a sqaure instance"""
 
-    def __init__(self, size=None):
+    def __init__(self, size):
         """This method instantiates the sqaure object"""
 
         self._size = size
