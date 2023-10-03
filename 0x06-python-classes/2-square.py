ebba4831dc7cf3dac41219098a48fdc0cdfc582e
EricoMartin <ericomartin.0785@gmail.com>
2023-10-03 09:09:07 -0700

    2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -8,7 +8,7 @@ class Square:
     def __init__(self, size=0):
         """This method instantiates the sqaure object"""
 
-        if isinstance(int, size):
+        if isinstance(size, int):
             self.__size = size
         else:
             raise TypeError("size must be an integer")
