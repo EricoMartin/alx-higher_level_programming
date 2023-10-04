2150e1ff1a8d9529b4184512519fb60d06acaa22
EricoMartin <ericomartin.0785@gmail.com>
2023-10-04 12:55:40 -0700

    0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -16,9 +16,9 @@ def add_integer(a, b=98):
     var_a = isinstance(a, int) or isinstance(a, float)
     var_b = isinstance(b, int) or isinstance(b, float)
 
-    if var_a and var_b:
-        return int(a) + int(b)
-    elif not var_a:
+    if not var_a:
         raise TypeError("a must be an integer")
     elif not var_b:
         raise TypeError("b must be an integer")
+    else:
+        return int(a) + int(b)
