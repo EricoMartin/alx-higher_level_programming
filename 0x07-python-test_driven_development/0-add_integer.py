6287f69b9f51c5247306dce5f81c6da8f54cc562
EricoMartin <ericomartin.0785@gmail.com>
2023-10-04 12:38:02 -0700

    0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,7 +3,15 @@
 
 
 def add_integer(a, b=98):
-    """ This is the function Declaration"""
+    """ This is the add_integer function Declaration  
+        
+        Args:
+            a: a number or float
+            b: another number or float
+
+        Returns: 
+            integer addition
+    """
 
     var_a = isinstance(a, int) or isinstance(a, float)
     var_b = isinstance(b, int) or isinstance(b, float)
