2d201d2d6738bc01d56c7a5277f8442833f93761
EricoMartin <ericomartin.0785@gmail.com>
2023-10-06 04:14:03 -0700

    locked class

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -2,7 +2,7 @@
 """
 101-locked_class Module
 
-This module optimizes memory by creating obj based on 
+This module optimizes memory by creating obj based on
 certain criteria.
 """
 
@@ -10,3 +10,7 @@ certain criteria.
 class LockedClass:
     """ LockedClass Declaration """
     __slots__ = ('first_name')
+
+    def __init__(self, first_name=None):
+        if first_name is not None:
+            self.first_name = first_name
