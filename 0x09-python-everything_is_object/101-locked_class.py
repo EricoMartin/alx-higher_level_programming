e8865745c92994a66db4e2c6a43f9bb51ef0968c
EricoMartin <ericomartin.0785@gmail.com>
2023-10-06 03:55:50 -0700

    locked class

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-""" This the LockedClass Module """
+"""
+    This the LockedClass Module
+"""
 
 
 class LockedClass:
     """ LockedClass Declaration """
     __slots__ = (first_name)
-    def __init__(self, first_name = None):
+
+    def __init__(self, first_name=None):
         """The init block"""
         if self.first_name is not None:
             self.first_name = first_name
