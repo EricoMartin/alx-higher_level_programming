fc83dd15cad855e4bdb7ba63ac2c5ac5c1435197
EricoMartin <ericomartin.0785@gmail.com>
2023-10-10 10:08:17 -0700

    6-base_geometry.py

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -8,6 +8,7 @@ class BaseGeometry:
     def __init__(self):
         """An empty init constructor"""
         pass
+
     def area(self):
         """raises an exception when called"""
         raise Exception("area() is not implemented")
