f03dfba9e529c485909b37f03a885f4880ec60aa
EricoMartin <ericomartin.0785@gmail.com>
2023-10-17 15:48:15 -0700

    unittest base

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -56,3 +56,12 @@ class Square(Rectangle):
                     self.x = v
                 elif k == "y":
                     self.y = v
+
+    def to_dictionary(self):
+        """ to dictionary function """
+
+        return {
+            "id": self.id,
+            "size": self.size,
+            "x": self.x,
+            "y": self.y }
