60f8ff1a2e33da8109366540d1ddcc43710bc370
EricoMartin <ericomartin.0785@gmail.com>
2023-11-26 10:20:12 -0800

    12-no_genre.sql

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -5,5 +5,5 @@ FROM tv_shows
 	LEFT JOIN tv_show_genres
 
 ON tv_show_genres.genre_id = tv_shows.id
-WHERE tv_show_genres.genre_id = NULL
+WHERE tv_show_genres.show_id = NULL
 ORDER BY tv_shows.title;
