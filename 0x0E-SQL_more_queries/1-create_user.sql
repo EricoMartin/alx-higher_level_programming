2e702e10079c0dc470350f0bb93a24fe72b4b295
EricoMartin <ericomartin.0785@gmail.com>
2023-11-26 06:42:10 -0800

    1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,5 @@
 -- A script that creates the MySQL server user user_0d_1d
 
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+FLUSH PRIVILEGES;
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTIONS;
