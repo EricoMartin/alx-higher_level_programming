ac4c9b461414cc8e5cb7fbae30cb1e96e396a199
EricoMartin <ericomartin.0785@gmail.com>
2023-11-26 09:22:25 -0800

    8-cities_of_california_subquery.sql

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- A script that lists all cities contained in the database hbtn_0d_usa.
 
 SELECT cities.id, cities.name, states.name
-FROM cities FULL OUTER JOIN states 
-USING (states.id);
+FROM cities RIGHT JOIN states
+WHERE cities.state_id = states.id
