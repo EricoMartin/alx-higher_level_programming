1cfe4dea6ebe96349d83aac86f2a4c7914288c65
EricoMartin <ericomartin.0785@gmail.com>
2023-11-26 10:00:13 -0800

    12-no_genre.sql

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,8 @@
+-- A script that lists all shows contained in hbtn_0d_tvshows without a genre linked.
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows 
+	JOIN tv_genres
+	JOIN tv_show_genres
+
+ON tv_show_genres.genre_id != tv_genres.id;
