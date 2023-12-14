626d11c4dd65b78fba8276744639bee91a888497
EricoMartin <ericomartin.0785@gmail.com>
2023-12-13 17:58:21 -0800

    1-multi_languages.js

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const myFunc = () => {
+  const first = 'C is fun';
+  const second = 'Python is cool';
+  const third = 'JavaScript is amazing';
+
+  console.log(first);
+  console.log(second);
+  console.log(third);
+};
+
+myFunc();
