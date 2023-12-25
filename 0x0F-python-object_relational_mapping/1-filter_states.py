0e509c7797cdb12464873fb408f40869599e787c
EricoMartin <ericomartin.0785@gmail.com>
2023-12-25 07:11:58 -0800

    1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-A python module that lists all states with
-a name starting with N from the database hbtn_0e_0_usa.
+A python module that lists all states with a name starting with N
+from the database hbtn_0e_0_usa.
 """
 
 import MySQLdb
