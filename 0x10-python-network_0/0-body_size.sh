4781e7e220a85e57aa10bc2a5a73e346208d0273
EricoMartin <ericomartin.0785@gmail.com>
2024-01-06 12:29:58 -0800

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #a Bash script that takes in a URL, sends a request to that URL
-curl -s -w "%{size_download}\n" -o /dev/null $0
+curl -s -w "%{size_download}\n" -o $1 | grep 'Content-Length' | awk '{print $2}'
