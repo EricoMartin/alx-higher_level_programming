d525fc3c22cc7f5298c95da249bf94383f9f5658
EricoMartin <ericomartin.0785@gmail.com>
2024-01-08 06:30:42 -0800

    6-main.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,12 +6,20 @@
 def find_peak(list_of_integers):
     """ This function returns the peak of the list
     """
-    if (len(list_of_integers) == 0):
+    if list_of_integers is None or list_of_integers == []:
         return None
-
-    else:
-        peak = list_of_integers[0]
-        for i in range(1, len(list_of_integers) - 1):
-            if list_of_integers[i] >= peak - 1:
-                peak = list_of_integers[i]
-        return peak
+    lo = 0
+    hi = len(list_of_integers)
+    mid = ((hi - lo) // 2) + lo
+    mid = int(mid)
+    if hi == 1:
+        return list_of_integers[0]
+    if hi == 2:
+        return max(list_of_integers)
+    if list_of_integers[mid] >= list_of_integers[mid - 1] and\
+            list_of_integers[mid] >= list_of_integers[mid + 1]:
+        return list_of_integers[mid]
+    if mid > 0 and list_of_integers[mid] < list_of_integers[mid + 1]:
+        return find_peak(list_of_integers[mid:])
+    if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
